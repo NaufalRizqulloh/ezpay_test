4b2d8f74a24f31e1f25da3b06045c55cc51be8ef
adh1ka <hafizhadhi27@gmail.com>
2025-12-01 06:30:59 +0700

    continued

--- lib/screens/customer/customer_qris_screen.dart
+++ lib/screens/customer/customer_qris_screen.dart
@@ -3,14 +3,13 @@
 import 'package:flutter/material.dart';
 import 'package:ezpay_test/components/box_decoration.dart';
 import 'package:ezpay_test/constants/app_colors.dart';
-import 'package:ezpay_test/screens/customer/customer_qris_screen.dart';
 
-class CustomerHomeScreen extends StatefulWidget {
+class CustomerQrisScreen extends StatefulWidget {
   @override
-  _CustomerHomeScreenState createState() => _CustomerHomeScreenState();
+  _CustomerQrisScreenState createState() => _CustomerQrisScreenState();
 }
 
-class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
+class _CustomerQrisScreenState extends State<CustomerQrisScreen> {
   void _scanQris() {
     Navigator.pushReplacement(
       context,
@@ -45,20 +44,20 @@ class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              ElevatedButton(
-                onPressed: _scanQris,
-                style: ElevatedButton.styleFrom(
-                  fixedSize: Size(120, 100),
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                ),
-                child: Text(
-                  'Scan QRIS',
-                  style: TextStyle(fontSize: 16, color: Colors.black),
-                ),
-              ),
+              // ElevatedButton(
+              //   onPressed: _processLogin,
+              //   style: ElevatedButton.styleFrom(
+              //     fixedSize: Size(1000, 48),
+              //     backgroundColor: AppColors.primary,
+              //     shape: RoundedRectangleBorder(
+              //       borderRadius: BorderRadius.circular(16),
+              //     ),
+              //   ),
+              //   child: Text(
+              //     'Continue   >',
+              //     style: TextStyle(fontSize: 16, color: Colors.white),
+              //   ),
+              // ),
               Container(
                 width: 120,
                 height: 100,
@@ -84,16 +83,6 @@ class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
                   ],
                 ),
               ),
-            ],
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(width: 2),
-              Text("My Digital Wallet"),
-              Text("Icon"),
-              SizedBox(width: 1),
             ],
           ),
         ],
